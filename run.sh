b4f7ec3a74686b67f9395d8431824c1fd4d26c26
Chih Chiu <chih.chiu.19@gmail.com>
2017-04-12 19:31:33 -0700

    better run.sh

--- run.sh
+++ run.sh
@@ -8,11 +8,14 @@ fi
 
 if [ x$@ == "x" ]; then
     echo
-    echo '------------------------------------------------------------'
-    echo 'To quit, press ctrl-c twice.'
-    echo '------------------------------------------------------------'
+    echo "------------------------------------------------------------"
+    echo "Browse page at http://${machine_ip}:8080"
+    echo "You can register it with any info."
+    echo "The save files will be in the 'saves' folder."
+    echo "To quit, press ctrl-c."
+    echo "------------------------------------------------------------"
     echo
-    docker run -it -p 8080:8080 -v $(pwd)/saves:/crawl/crawl-ref/source/saves chihchiu29/docker_roguelike | sed s/localhost/${machine_ip}/g
+    docker run -it -p 8080:8080 -v $(pwd)/saves:/crawl/crawl-ref/source/saves chihchiu29/docker_roguelike
 else
     docker run -it -p 8080:8080 -v $(pwd)/saves:/crawl/crawl-ref/source/saves chihchiu29/docker_roguelike "$@"
 fi
